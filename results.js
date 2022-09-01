92d3941666eaad9469f6429f4599027eea1eedf3
Mariam-Labers <69308145+Mariam-Labers@users.noreply.github.com>
2022-09-01 15:33:13 +0000

    Add files via upload

---
+++ results.js
@@ -0,0 +1,9 @@
+const results = [
+  { name: "Adams Gamson", class: "form three", grade: "8" },
+  { name: "Yenko Peter", class: "form three", grade: "25" },
+  { name: "Adams Atif", class: "form three", grade: "38" },
+  { name: "Damsel Mariam", class: "form three", grade: "9" },
+  { name: "Suburu Marcus", class: "form three", grade: "18" },
+];
+
+module.exports = results;
